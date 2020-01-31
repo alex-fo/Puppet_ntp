198a6ba3b7a2d944597ca39355fd02b47094ba69
Alexandre <alex@fournot.com>
2020-01-31 03:51:50 -0500

    Update all content

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class p-pbg_ntp {
   ensure_packages(['ntp'])
 
   file { '/etc/ntp.conf':
-    source => 'puppet:///modules/pp-pbg_ntp/ntp.conf',
+    source => 'puppet:///modules/p-pbg_ntp/ntp.conf',
     notify => Service['ntp'],
     require => Package['ntp'],
   }
